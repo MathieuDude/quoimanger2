5461fe57ffd1faa9255c608ba4d5179dc3bee59d
Mathieu Labelle <mlabelle1999@hotmail.com>
2020-10-07 22:18:32 -0400

    Firebase setup

---
+++ ApiKeys.js
@@ -0,0 +1,10 @@
+export default { firebaseConfig: {
+    apiKey: "AIzaSyA9H3KXu2aaV3rvJYcl81K40O8Vvn7RcAw",
+    authDomain: "quoi-manger-8c96c.firebaseapp.com",
+    databaseURL: "https://quoi-manger-8c96c.firebaseio.com",
+    projectId: "quoi-manger-8c96c",
+    storageBucket: "quoi-manger-8c96c.appspot.com",
+    messagingSenderId: "308285374121",
+    appId: "1:308285374121:web:376afb494a75a9ef7bf3b8",
+    measurementId: "G-5RWKYXDL2G"
+}};
--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
 
-export default function App() {
-  return (
+import ApiKeys from './ApiKeys';
+
+firebase.initializeApp(ApiKeys.firebaseConfig);
+
+
+export default class App extends React.Component {
+
+  constructor(props)
+  {
+    super(props);
+    
+    this.state= {
+      isLoadingComplete: false,
+    }
+
+    if(!firebase.apps.length) {firebase.initializeApp(ApiKeys.firebaseConfig);}
+
+  }
+
+
+  render (){
+    return(
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
-  );
+    </View>);
+  }
 }
 
 const styles = StyleSheet.create({
