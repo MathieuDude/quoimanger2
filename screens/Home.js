f2e53160576f0d16fefe8f0671bf62473556f21e
Mathieu Labelle <mlabelle1999@hotmail.com>
2020-10-14 00:17:26 -0400

    Loading de salon fonctionnel

--- screens/Home.js
+++ screens/Home.js
@@ -11,67 +11,72 @@ import * as firebase from 'firebase';
 if(!firebase.apps.length) {firebase.initializeApp(ApiKeys.firebaseConfig);}
 
 //initialize DB
+const dbh = firebase.firestore();
 //TODO: linker les données avec la BD pour les afficher dans la flatlist
+
 const DATA = [
-  {
-    salonId: 1,
-    title: 'Salon de Jul',
-  },
+    {
+        salonId: 2,
+        title: 'Salon de Jul',
+    }
 ];
 
-//FONCTIONNEL PLUS OU MOINS
-
-//.where("active", "==", true)
-//GETS THE LOBBIES AND PUSHES THEM TO DATA
-
-
-//REWRITE WITH A CLASS??
-
-
 const Home = (navigation) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState([]);
 
-    const dbh = firebase.firestore();
+    var tempDATA = DATA;
 
-    dbh.collection("lobbies")
+    if(isLoading == true){
+        dbh.collection("lobbies")
         .get()
         .then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
-    
-                // setIsLoading(true);
-                // console.log(doc.id, " => ", doc.data());
-                DATA.concat(doc.data());
-                console.log('FUCK');
-    
+                console.log('doc.data:');
+                console.log(doc.data());
+                //this does not work obviously
+                tempDATA.push({   
+                    "salonId": doc.get('salonId'),
+                    "title": doc.get('title')
+                });
+                    
                 
+
+                console.log('tempData:');
+                console.log(tempDATA); 
+
+                setData(tempDATA);
+
             });
-            // setIsLoading(false);
+            setIsLoading(false)
+            
         })
         .catch(function(error) {
             console.log("Error getting documents: ", error);
         });
 
-    const [isLoading, setIsLoading] = useState(false);
+        console.log('DATA FINAL:');
+        console.log(data);
+        
+    }
 
     
 
-    const [useableData, setData] = useState(DATA);
 
-    console.log(useableData);
-
-    if(isLoading){
-        return(
-          <View style={styles.preloader}>
-            <ActivityIndicator size="large" color="#9E9E9E"/>
-          </View>
-        )
-    }
-    else{
+    // if(isLoading){
+    //     return(
+    //       <View style={styles.preloader}>
+    //         <ActivityIndicator size="large" color="#9E9E9E"/>
+    //       </View>
+    //     )
+    // }
+    // else{
         return(
             <View style={styles.container}>
                 <StatusBar style="auto" />
                 <Text style={styles.sousTitre}>Salons</Text>
                 <FlatList
-                    data={DATA}
+                    data={tempDATA}
                     keyExtractor={item => item.salonId}
                     renderItem={({item}) =>
                     <SalonItem salonItem={item} nav={navigation}/>
@@ -80,7 +85,7 @@ const Home = (navigation) => {
             </View>
         );
 
-    }
+    // }
     
 
     
