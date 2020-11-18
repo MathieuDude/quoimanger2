de5fc09717c6305ddeeccdc96179b1ec93a12193
Mathieu Labelle <mlabelle1999@hotmail.com>
2020-11-18 01:59:39 -0500

    added GPS

--- screens/PreCreationSalon.js
+++ screens/PreCreationSalon.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import { Text, View, TextInput, TouchableOpacity, Dimensions } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, Dimensions, Alert } from 'react-native';
 import MapView, {Polygon, Polyline, Marker} from 'react-native-maps';
 import styles from '../styles';
 import ApiKeys from '../ApiKeys';
 import Slider from '@react-native-community/slider';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+
 
 //NOTES: https://docs.expo.io/versions/v39.0.0/sdk/location/
 //https://docs.expo.io/versions/v39.0.0/sdk/permissions/
@@ -11,48 +15,60 @@ import Slider from '@react-native-community/slider';
 
 
 const CreationSalon = ({route, navigation}) => {
-  const [sliderValue, setSliderValue] = useState(5);
+    const [sliderValue, setSliderValue] = useState(5);
+    const [userLocation, setUserLocation] = useState({});
+    const coord1 = {
+        "latitude": 45.6422237,
+        "longitude": -73.8446587,
+        "latitudeDelta": 0.7755658183766698,
+        "longitudeDelta": 0.6969268496238357,
+    };
+    const _getLocation = async () => {
+        var { status } = {};
+        if(status != 'granted')
+        {
+            status = await Permissions.askAsync(Permissions.LOCATION);
+        }
+        const userLoc = Location.getCurrentPositionAsync();
+        setUserLocation(userLoc);
+        console.log(userLocation);
+    };
 
-  const coord1 = {
-    "latitude": 45.6422237,
-    "longitude": -73.8446587,
-    "latitudeDelta": 0.7755658183766698,
-    "longitudeDelta": 0.6969268496238357,
-  }
-
-  return(
-      <View style={styles.formContainer}>
-        <MapView  initialRegion={coord1}
-            style={{
-                width: Dimensions.get('window').width,
-                height: Dimensions.get('window').height*0.7
-            }}>
-        </MapView>
-        <Text style={styles.formLabel}>Rayon de recherche: </Text>
-        <Slider
-          style={{width: 300, height: 40}}
-          minimumValue={0}
-          maximumValue={1}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-          thumbTintColor="#28ADEA"
-          onValueChange={(value) => setSliderValue(value)}
-          value={5}
-          step={0.5}
-          minimumValue={0.5}
-          maximumValue={20}
-        />
-        <Text>Rayon de recherche: {sliderValue}km</Text>
-        <TouchableOpacity style={styles.buttonContainer} onPress={() => {
-
-            
-                navigation.navigate('CreationSalon', {searchRadius: sliderValue})
-
-            }}>
-            <Text style={styles.buttonBlue}>Passé à la prochaine étape</Text>
-        </TouchableOpacity>
-      </View>
-  );
+    return(
+        <View style={styles.formContainer}>
+            <MapView  initialRegion={coord1}
+                style={{
+                    width: Dimensions.get('window').width,
+                    height: Dimensions.get('window').height*0.7
+                }}>
+            </MapView>
+            <Text style={styles.formLabel}>Rayon de recherche: </Text>
+            <Slider
+                style={{width: 300, height: 40}}
+                minimumValue={0}
+                maximumValue={1}
+                minimumTrackTintColor="#FFFFFF"
+                maximumTrackTintColor="#000000"
+                thumbTintColor="#28ADEA"
+                onValueChange={(value) => setSliderValue(value)}
+                value={5}
+                step={0.5}
+                minimumValue={0.5}
+                maximumValue={20}
+            />
+            <Text>Rayon de recherche: {sliderValue}km</Text>
+            <TouchableOpacity style={styles.buttonContainer} onPress={() => {
+                    navigation.navigate('CreationSalon', {searchRadius: sliderValue})
+                }}>
+                <Text style={styles.buttonBlue}>Passé à la prochaine étape</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer} onPress={() => {
+                    _getLocation();
+                }}>
+                <Text style={styles.buttonBlue}>GPS</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 export default CreationSalon;
